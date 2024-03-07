655634e0951bc23881fcf5e2c4ec37abad47a992
guille9492 <150793115+guille9492@users.noreply.github.com>
2024-03-07 16:39:50 +0100

    Create insertar-usuarios.php

---
+++ insertar-usuarios.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form id="formulibro" name="formulibro" method="post">
+  <p>
+    <label for="Nombre">Nombre:</label>
+    <input type="text" name="Nombre" id="Nombre">
+    <label for="Email">Email:</label>
+    <input type="text" name="Email" id="Email">
+    <label for="Telefono">Telefono:</label>
+    <input type="text" name="Telefono" id="Telefono">
+  </p>
+  
+  <p>
+    <input name="Enviar" type="submit" id="Enviar" formaction="procesa.php" value="Enviar">
+  </p>
+</body>
+</html>
