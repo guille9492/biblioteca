e30ea43cefc33659c2babe046ff47a29a537a0dd
[guille9492] <[guillermogonzalezmiranda@gmail.com]>
2024-03-08 11:53:40 +0100

    modified:   insertar-libros.php 	modified:   insertar-usuarios.php

--- insertar-libros.php
+++ insertar-libros.php
@@ -4,9 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-<form id="formulibro" name="formulibro" method="post">
+<div class="container mt-5">
+  <h2 class="mb-4">Agregar Libro</h2>
+  <form id="formulibro" name="formulibro" method="post">
+    <div class="form-group">
+      <label for="Titulo">Título:</label>
+      <input type="text" name ="Titulo" class="form-control" id="Titulo">
+    </div>
+    <div class="form-group">
+      <label for="Autor">Autor:</label>
+      <input type="text" class="form-control" id="Autor" name="Autor">
+    </div>
+    <div class="form-group">
+      <label for="Genero">Género:</label>
+      <select class="form-control" id="Genero" name="Genero">
+        <option>Ficción</option>
+        <option>No ficción</option>
+        <option>Poesía</option>
+        <option>Historia</option>
+        <option>Ciencia ficción</option>
+        <!-- Agrega más opciones según sea necesario -->
+      </select>
+    </div>
+    <div class="form-group">
+      <label for="isbn">ISBN:</label>
+      <input type="text" class="form-control" id="isbn" name="isbn">
+    </div>
+    <div class="form-group">
+      <label for="Editorial">Editorial:</label>
+      <input type="text" class="form-control" id="Editorial" name="Editorial">
+    </div>
+    <div class="form-group">
+      <label for="Cantidad">Cantidad:</label>
+      <input type="text" class="form-control" id="Cantidad" name="Cantidad">
+    </div>
+    <button type="submit" class="btn btn-primary" name="Enviar" id="Enviar" formaction="procesa.php">Agregar Libro</button>
+  </form>
+</div>
+
+
+<!--<form id="formulibro" name="formulibro" method="post">
   <p>
     <label for="Titulo">Titulo:</label>
     <input type="text" name="Titulo" id="Titulo" required>
@@ -21,9 +61,10 @@
     <label for="Cantidad">Cantidad:</label>
     <input type="text" name="Cantidad" id="Cantidad" required>
   </p>
-  
+
   <p>
     <input name="Enviar" type="submit" id="Enviar" formaction="procesa.php" value="Enviar">
   </p>
+  -->
 </body>
 </html>
--- insertar-usuarios.php
+++ insertar-usuarios.php
@@ -4,8 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
+
+<div class="container mt-5">
+  <h2 class="mb-4">Agregar Usuario</h2>
+  <form id="formuusuario" name="formuusuario" method="post">
+    <div class="form-group">
+      <label for="Nombre">Nombre:</label>
+      <input type="text" name ="Nombre" class="form-control" id="Nombre">
+    </div>
+    <div class="form-group">
+      <label for="Email">Email:</label>
+      <input type="text" class="form-control" id="Email" name="Email">
+    </div>
+    <div class="form-group">
+      <label for="Telefono">Telefono:</label>
+      <input type="text" class="form-control" id="Telefono" name="Telefono">
+    </div>
+    <button type="submit" class="btn btn-primary" name="Enviar" id="Enviar" formaction="procesa.php">Agregar Usuario</button>
+  </form>
+</div>
+
+
+
+
+ <!-- 
 <form id="formulibro" name="formulibro" method="post">
   <p>
     <label for="Nombre">Nombre:</label>
@@ -19,5 +44,6 @@
   <p>
     <input name="Enviar" type="submit" id="Enviar" formaction="procesa.php" value="Enviar">
   </p>
+-->
 </body>
 </html>
